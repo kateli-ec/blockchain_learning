93cff502c67cb3ffcf0e0951e3190938cf30d9a3
Kate Li <kateli@Kates-MacBook-Pro-2.local>
2024-03-29 11:18:04 -0700

    hardhat

---
+++ hardhat-tutorial/test/Token.js
@@ -0,0 +1,14 @@
+// the ethers variable is available in the global scope. could include following line to be explicit
+// const { ethers } = require("hardhat");
+const { expect } = require("chai");
+
+describe("Token contract", function () {
+  it("Deployment should assign the total supply of tokens to the owner", async function () {
+    const [owner] = await ethers.getSigners();
+
+    const hardhatToken = await ethers.deployContract("Token");
+
+    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+  });
+});
